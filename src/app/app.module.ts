7932719ceeb80bd4faf4dfe3bc0dd47548e732f5
Dmitriy Sideltsev <greedline74@gmail.com>
2021-02-25 04:24:46 +0500

    Update
    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    PartnersComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
